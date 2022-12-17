e6ac6f6fd8eadb767261084ef0f8d88ff69a144b
Théo Prigent <dev@tprigent.fr>
2022-12-17 18:25:00 +0000

    Function to extract keypoints from image

---
+++ extract_keypoints.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+
+def extract_kp_des(image_name, n_kpoints):
+    # Load the two images and convert them to grayscale
+    image = cv2.imread(image_name)
+
+    # Convert images to array
+    im_array = np.array(image)
+    im_array = im_array.astype(np.uint8)
+
+    # apply SIFT to both images to get key points and descriptors
+    sift = cv2.SIFT_create(n_kpoints)
+    key_points, descriptors = sift.detectAndCompute(im_array, None)
+
+    return key_points, descriptors
+
+
+
+
