01f2a8f596899e194de3694045c1376fc83c0058
Théo Prigent <dev@tprigent.fr>
2022-12-29 11:55:56 +0100

    Avoid matching multiple times the same keypoints

--- kp_matching.py
+++ kp_matching.py
@@ -6,6 +6,7 @@ def brute_force_matcher(descriptors1, descriptors2):
     n_desc2 = descriptors2.shape[0]
 
     correspondences = []
+    used = np.zeros(n_desc2)
 
     # Descriptors of the 1st set
     for i in range(n_desc1):
@@ -16,17 +17,20 @@ def brute_force_matcher(descriptors1, descriptors2):
 
         # Descriptors for the 2nd set
         for j in range(n_desc2):
-            descriptor2 = descriptors2[j]
+            if used[j] != 1:
+                descriptor2 = descriptors2[j]
 
-            # Calculate the distance between the descriptors
-            distance = np.linalg.norm(descriptor1 - descriptor2)
+                # Calculate the distance between the descriptors
+                distance = np.linalg.norm(descriptor1 - descriptor2)
 
-            # Check if j is better candidate
-            if distance < min_distance:
-                best_match = j
-                min_distance = distance
+                # Check if j is better candidate
+                if distance < min_distance:
+                    best_match = j
+                    min_distance = distance
 
         # Update result list with best candidate
-        correspondences.append((i, best_match))
+        if best_match is not None:
+            correspondences.append((i, best_match))
+            used[best_match] = 1
 
     return correspondences
