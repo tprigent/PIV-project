292ba0f636e5c76c0def2b78776bd7af105ceff1
Théo Prigent <dev@tprigent.fr>
2023-01-10 19:07:57 +0000

    [In progress] Update file organization + using given keypoints

--- kp_matching.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from scipy.spatial import cKDTree
-
-
-def distance_matcher(descriptors1, descriptors2):
-    tree = cKDTree(descriptors1)
-    _, matches = tree.query(descriptors2)
-
-    return matches
--- pivproject2022_task1.py
+++ pivproject2022_task1.py
@@ -1,15 +1,61 @@
-from scipy.ndimage.interpolation import shift
+import sys
+import os
+import scipy
 import numpy as np
-import random
 from tqdm import tqdm
-import cv2
 
 
-def ransac(keypoints1, keypoints2, n_iter, n_data, th, n_valid):
-    # Convert the keypoints to points
-    kpts1 = [keypoints1[i].pt for i in range(len(keypoints1))]
-    kpts2 = [keypoints2[i].pt for i in range(len(keypoints2))]
+def compute_homography_for_dataset(template_path, input_dir, output_dir):
+    extensions = ['.jpg', '.jpeg', '.png']
+    input_files = [f for f in os.listdir(input_dir) if any(f.endswith(ext) for ext in extensions)]
 
+    # unpacking template data
+    template_name = os.path.splitext(template_path)[0]
+    template_mat = scipy.io.loadmat(template_name + '.mat')
+    kp_template = np.array(template_mat['p'][:200])
+    desc_template = np.array(template_mat['d'][:200])
+
+    iteration_counter = 1
+
+    for input_name_ext in tqdm(input_files):
+
+        input_name = os.path.splitext(input_name_ext)[0]
+
+        # unpacking input data
+        input_mat = scipy.io.loadmat(input_dir + input_name + '.mat')
+        kp_input = np.array(input_mat['p'][:200])
+        desc_input = np.array(input_mat['d'][:200])
+
+        # raw matching descriptors
+        correspondences = descriptor_matcher(descriptors1=desc_template.T, descriptors2=desc_input.T)
+        match_template = [correspondences[i] for i in range(len(correspondences))]
+        match_input = [i for i in range(len(correspondences))]
+
+        # get corresponding matched keypoints
+        kp_t_match = [kp_template.T[int(i)] for i in match_template]
+        kp_i_match = [kp_input.T[int(i)] for i in match_input]
+
+        # select good matches and compute homography
+        H = None
+        tolerance = 0
+        while H is None:
+            H, inliers_index = ransac(kp_t_match, kp_i_match, n_iter=100, n_data=4, th=3+tolerance, n_valid=20)
+            tolerance += 1
+
+        H_mat_name = 'H_{}.mat'.format(str(iteration_counter).zfill(4))
+        scipy.io.savemat(output_dir + H_mat_name, {'H': H})
+        iteration_counter += 1
+
+
+def descriptor_matcher(descriptors1, descriptors2):
+
+    tree = scipy.spatial.cKDTree(descriptors1)
+    _, matches = tree.query(descriptors2)
+
+    return matches
+
+
+def ransac(kpts1, kpts2, n_iter, n_data, th, n_valid):
     # memory
     best_fit = None
     min_error = np.inf
@@ -18,7 +64,7 @@ def ransac(keypoints1, keypoints2, n_iter, n_data, th, n_valid):
     # iterating n times
     for i in tqdm(range(n_iter)):
         # taking n random keypoints in each side
-        sample_indexes = random.sample(range(len(keypoints1)), n_data)
+        sample_indexes = np.random.choice(range(len(kpts1)), n_data, replace=False)
         candidates1 = [kpts1[i] for i in sample_indexes]
         candidates2 = [kpts2[i] for i in sample_indexes]
 
@@ -26,8 +72,6 @@ def ransac(keypoints1, keypoints2, n_iter, n_data, th, n_valid):
         model = compute_homography(candidates1, candidates2)
 
         # find inliers
-        #mse = measure_accuracy(model, candidates1, candidates2)
-        #print('mse = {}'.format(mse))
         inliers1, inliers2, inlier_indexes = [], [], []
         for j in range(len(kpts1)):
             if distance(model, kpts1[j], kpts2[j]) < th:
@@ -94,4 +138,20 @@ def compute_error(homography, points1, points2):
         err += distance(homography, points1[i], points2[i])
 
     return err
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) > 3:
+        template_dir = args[1]
+        input_dir = args[2]
+        output_dir = args[3]
+    else:
+        template_dir = 'template/templateSNS.jpg'
+        input_dir = 'data/'
+        output_dir = 'results/'
+
+    compute_homography_for_dataset(template_dir, input_dir, output_dir)
+
+
 
